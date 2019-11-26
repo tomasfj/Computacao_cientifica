77d9b7eb3beaed70569d8c178cf58e400a5aa392
samot <tomas.jeronimo@ubi.pt>
2019-11-26 15:26:02 +0000

    Added File functions.py
    Added a file to create some test functions before applying to the main project

---
+++ functions.py
@@ -0,0 +1,52 @@
+class utente:
+    def __init__(self, prioridade, horaEntrada, saida_fase1, saida_fase2, saida_fase3, balcao_Fase2, tempoAt_Balcao_Fase1, tempoAt_BalcaoA_Fase2, tempoAt_BalcaoB_Fase2, tempoAt_BalcaoC_Fase2, tempoAt_Balcao_Fase3):
+        self.prioridade = prioridade
+        self.horaEntrada = horaEntrada
+        self.saida_fase1 = saida_fase1
+        self.saida_fase2 = saida_fase2
+        self.saida_fase3 = saida_fase3
+        self.balcao_Fase2 = balcao_Fase2
+        self.tempoAt_Balcao_Fase1 = tempoAt_Balcao_Fase1
+        self.tempoAt_BalcaoA_Fase2 = tempoAt_BalcaoA_Fase2
+        self.tempoAt_BalcaoB_Fase2 = tempoAt_BalcaoB_Fase2
+        self.tempoAt_BalcaoC_Fase2 = tempoAt_BalcaoC_Fase2
+        self.tempoAt_Balcao_Fase3 = tempoAt_Balcao_Fase3
+
+
+# prioridade (0 => geral, 1 => prioritario pos pagamento, 2 => prioritario geral)  --NOT NULL  --PODE MUDAR DURANTE O PROGRAMA
+# horaEntrada (entre 9h e 17h)  --NOT NULL
+# saida_fase1 (2 => vai para fase 2, 3 => vai para fase 3(saida_fase3 = 0) )  --NOT NULL
+# saida_fase2 (0 => sai do sistema, 3 => vai para fase 3(saida_fase3 = 1) )   --NOT NULL
+# saida_fase3 (0 => sai do sistema, 2 => vai para fase 2(saida_fase2 = 0) )   --NOT NULL
+# balcao_Fase2 (1 => balcao A, 2 => balcao B, 3 => balcao C)
+# tempoAt_Balcao_Fase1 (entre 0 min e 3 min)     --NOT NULL
+# tempoAt_BalcaoA_Fase2 (entre 0 min e 30 min)   
+# tempoAt_BalcaoB_Fase2 (entre 0 min e 20 min)   
+# tempoAt_BalcaoC_Fase2 (entre 0 min e 20 min)   
+# tempoAt_Balcao_Fase3 (entre 0 min e 3 min)
+
+# exmeplo: utente1 = utente(0, 9, 2, 0, 1, 1, 2, 3, 4, 5);
+
+# FILAS DA FASE 1
+list_Fase1_Geral = []
+list_Fase1_Prioritaria = []
+
+# Adicionar o novo utente a uma das filas da Fase 1 (adiciona ao fim das listas)
+def entradaNoSistema(utente):
+    if(utente.prioridade == 0):
+        list_Fase1_Geral.append(utente)
+    elif (utente.prioridade == 2):
+        list_Fase1_Prioritaria.append(utente)
+
+
+# Retorna o próximo utente a ser atendido na Fase 1
+def escolherAt_Fase1():
+    if(not list_Fase1_Prioritaria):            # se a fila prioritaria estiver vazia
+        if(not list_Fase1_Geral):
+            return(-1)                         # lista geral e lista prioritaria vazias
+        else:
+            return(list_Fase1_Geral.pop(0))    # devovle primeiro index da lista geral
+    else:
+        return(list_Fase1_Prioritaria.pop(0))  # devovle primeiro index da lista prioritaria
+
+
