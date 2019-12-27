538a2ad3375d508dafb2313d2646ef30f893586a
Joao_o_girassol <jpcruzbrito@hotmail.com>
2019-12-27 23:47:40 +0000

    Sistema quase a funcionar

--- functions.py
+++ functions.py
@@ -1,86 +1,230 @@
-import numpy as np
-#oioioio
+import random
 
+# CLASSE DOS UTENTES
+# ---------------------------------------------------------------------------------------------------------------------------------------------
 class Utente:
-    def __init__(self, prioridade, horaEntrada, saida_fase1, saida_fase2, saida_fase3, balcao_Fase2, tempoAt_Balcao_Fase1, tempoAt_BalcaoA_Fase2, tempoAt_BalcaoB_Fase2, tempoAt_BalcaoC_Fase2, tempoAt_Balcao_Fase3):
-        self.prioridade = prioridade
-        self.horaEntrada = horaEntrada
-        self.saida_fase1 = saida_fase1
-        self.saida_fase2 = saida_fase2
-        self.saida_fase3 = saida_fase3
-        self.balcao_Fase2 = balcao_Fase2
-        self.tempoAt_Balcao_Fase1 = tempoAt_Balcao_Fase1
-        self.tempoAt_BalcaoA_Fase2 = tempoAt_BalcaoA_Fase2
-        self.tempoAt_BalcaoB_Fase2 = tempoAt_BalcaoB_Fase2
-        self.tempoAt_BalcaoC_Fase2 = tempoAt_BalcaoC_Fase2
-        self.tempoAt_Balcao_Fase3 = tempoAt_Balcao_Fase3
+    
+    def __init__(self, id, prioridade, hora_entrada, tempos):
+        self.id = id # identificador do utente
+        self.prioridade = prioridade # prioridade (0 - geral, 1 - prioritário pós pagamento, 2 - prioritário geral)
+        self.hora_entrada = hora_entrada
+        self.chegadas_filas = {} # instantes de chegada às filas (ex: {"fila_fase_1": None, "fila_fase_2_A": None, "fila_fase_3": None})
+        self.tempos = {} # todas as ações do utente e os tempos (ex: {"tr": None, "A": None, "te": None})
+        self.estado_atual = "antes" # das ações que vai realizar, esta é a que o utente está a realizar num dado momento
+        self.tempo_de_espera = 0
+        self.direto = False
 
+    def str(self): # retorna uma string que representa o objeto (utente)
+        aux = ""
+        for k,v in self.tempos.items():
+            aux += k + ": " + str(v) + ", "
+        aux = aux[:-2]
+        return "ID: {:<3} | Prioridade: {:<2} | Entrada: {:<6} | Tempos: {:<30}".format(self.id,self.prioridade,self.hora_entrada,aux)
+# ---------------------------------------------------------------------------------------------------------------------------------------------
 
-# prioridade (0 => geral, 1 => prioritario pos pagamento, 2 => prioritario geral)  --NOT NULL  --PODE MUDAR DURANTE O PROGRAMA
-# horaEntrada (entre 9h e 17h)  --NOT NULL
-# saida_fase1 (2 => vai para fase 2, 3 => vai para fase 3(saida_fase3 = 0) )  --NOT NULL
-# saida_fase2 (0 => sai do sistema, 3 => vai para fase 3(saida_fase3 = 1) )   --NOT NULL
-# saida_fase3 (0 => sai do sistema, 2 => vai para fase 2(saida_fase2 = 0) )   --NOT NULL
-# balcao_Fase2 (1 => balcao A, 2 => balcao B, 3 => balcao C)
-# tempoAt_Balcao_Fase1 (entre 0 min e 3 min)     --NOT NULL
-# tempoAt_BalcaoA_Fase2 (entre 0 min e 30 min)   
-# tempoAt_BalcaoB_Fase2 (entre 0 min e 20 min)   
-# tempoAt_BalcaoC_Fase2 (entre 0 min e 20 min)   
-# tempoAt_Balcao_Fase3 (entre 0 min e 3 min)
+# FUNÇÕES DE SUPORTE
+# -------------------------------------------------------------------------------------------------------------------------------------------
+def total_utentes(): # calcula o número total de utentes a modelar no sistema
+    
+    return(random.randint(120,150))
 
-# exmeplo: utente1 = utente(0, 9, 2, 0, 1, 1, 2, 3, 4, 5);
+def gerar_utentes(num_utentes): # gerar todos os utentes que o sistema vai modelar
+    
+    utentes = []
+    id_count = 1
+    num_aux = 0
 
-# FILAS FASE 1
-list_Fase1_Geral = [];
-list_Fase1_Prioritaria = []
-# FILAS FASE 2
-list_Fase2_BalcaoA = []
-list_Fase2_BalcaoB = []
-list_Fase2_BalcaoC = []
+    # FASE 1
+    # -----------------------------------------------------------------------------------------------
+    for i in range(num_utentes):
 
-# Adicionar o novo utente a uma das filas da Fase 1 (adiciona ao fim das listas)
-def entradaNoSistema(utente):
-    if(utente.prioridade == 0):
-        list_Fase1_Geral.append(utente)
-    elif (utente.prioridade == 2):
-        list_Fase1_Prioritaria.append(utente)
+        # gerar a prioridade
+        if(random.randint(1,10)<=2): rand_prioridade = 2
+        else: rand_prioridade = 0
 
+        num_aux = random.randint(1,100)
 
-# Retorna o proximo utente a ser atendido na Fase 1
-def escolherAt_Fase1():
-    if(not list_Fase1_Prioritaria):            # se a fila prioritaria estiver vazia
-        if(not list_Fase1_Geral):
-            return(-1)                         # lista geral e lista prioritaria vazias
+        # gerar a hora de entrada no sistema
+        if(num_aux<=10): # 10% dos utentes
+            rand_hora_entrada = random.randint(0,7200)
+        elif(num_aux>10 and num_aux<=35): # 25% dos utentes
+            rand_hora_entrada = random.randint(7201,14400)
+        elif(num_aux>35 and num_aux<=80): # 45% dos utentes
+            rand_hora_entrada = random.randint(14401,21600)
+        else: # 20% dos utentes
+            rand_hora_entrada = random.randint(21601,28800)
+
+        utentes.append(Utente(id_count,rand_prioridade,rand_hora_entrada,{}))
+        id_count += 1
+
+    # tempo no balcão de triagem
+    triagem_rapida = random.sample(utentes, int(0.55*num_utentes))
+    triagem_media = random.sample(list(set(utentes)-set(triagem_rapida)),int(0.35*num_utentes))
+    triagem_lenta = list(set(utentes)-set(triagem_rapida)-set(triagem_media))
+
+    for i in triagem_rapida: # atribuir tempos aos utentes que fazem uma triagem rápida
+        i.tempos.update({"tr":random.randint(0,60)})
+
+    for i in triagem_media: # atribuir tempos aos utentes que fazem uma triagem média
+        i.tempos.update({"tr":random.randint(60,120)})
+    
+    for i in triagem_lenta: # atribuir tempos aos utentes que fazem uma triagem lenta
+        i.tempos.update({"tr":random.randint(120,180)})
+    # -----------------------------------------------------------------------------------------------
+
+    # FASE 2
+    # -----------------------------------------------------------------------------------
+    # escolher que utentes continuam para a fase 2 e quais vão diretamente para a fase 3
+    fase_3_direta = random.sample(utentes, int(0.10*num_utentes))
+    for i in fase_3_direta: i.direto = True
+
+    fase_2 = list(set(utentes)-set(fase_3_direta))
+
+    balcoes_A = random.sample(fase_2, int(0.35*(len(fase_2))))
+    balcoes_B = random.sample(list(set(fase_2)-set(balcoes_A)),int(0.50*(len(fase_2))))
+    balcoes_C = list(set(fase_2)-set(balcoes_A)-set(balcoes_B))
+
+    for i in balcoes_A: # atribuir tempos aos utentes que vão para os balcões A
+        num_aux = random.randint(1,100)
+        if(num_aux<=25): i.tempos.update({"A":random.randint(0,300)})
+        elif(num_aux>25 and num_aux<=60): i.tempos.update({"A":random.randint(300,900)})
+        elif(num_aux>60 and num_aux<=90): i.tempos.update({"A":random.randint(900,1500)})
+        else: i.tempos.update({"A":random.randint(1500,1800)})
+
+    for i in balcoes_B: # atribuir tempos aos utentes que vão para os balcões B
+        num_aux = random.randint(1,100)
+        if(num_aux<=25): i.tempos.update({"B":random.randint(0,300)})
+        elif(num_aux>25 and num_aux<=70): i.tempos.update({"B":random.randint(300,600)})
+        elif(num_aux>70 and num_aux<=95): i.tempos.update({"B":random.randint(600,900)})
+        else: i.tempos.update({"B":random.randint(900,1200)})
+
+    for i in balcoes_C: # atribuir tempos aos utentes que vão para os balcões B
+        num_aux = random.randint(1,100)
+        if(num_aux<=10): i.tempos.update({"C":random.randint(0,300)})
+        elif(num_aux>10 and num_aux<=45): i.tempos.update({"C":random.randint(300,600)})
+        elif(num_aux>45 and num_aux<=90): i.tempos.update({"C":random.randint(600,900)})
+        else: i.tempos.update({"C":random.randint(900,1200)})
+    # -----------------------------------------------------------------------------------
+
+    # FASE 3
+    # ----------------------------------------------------------------------------------
+    # escolher que utentes continuam para a fase 3
+    fase_3_A = random.sample(balcoes_A, int(0.20*len(balcoes_A)))
+    fase_3_B = random.sample(balcoes_B, int(0.30*len(balcoes_B)))
+    fase_3_C = random.sample(balcoes_C, int(0.75*len(balcoes_C)))
+
+    fase_3_combinada = fase_3_A + fase_3_B + fase_3_C + fase_3_direta
+
+    for i in fase_3_combinada:
+        num_aux = random.randint(1,100)
+        if(num_aux<=40): i.tempos.update({"te":random.randint(0,60)})
+        elif(num_aux>40 and num_aux<=95): i.tempos.update({"te":random.randint(60,120)})
+        else: i.tempos.update({"te":random.randint(120,180)})
+
+    # tratar dos utentes que voltam para a fase 2
+    fase_3_A_voltam = random.sample(fase_3_A, int(0.30*len(fase_3_A)))
+    fase_3_B_voltam = random.sample(fase_3_B, int(0.20*len(fase_3_B)))
+    fase_3_C_voltam = random.sample(fase_3_C, int(0.40*len(fase_3_C)))
+
+    tempo_aux = 0
+    for i in fase_3_A_voltam:
+        i.prioridade = 1
+        tempo_aux = i.tempos.get("A")
+        i.tempos.update({"A":random.randint(1,tempo_aux)})
+        i.tempos.update({"A_volta":tempo_aux-i.tempos.get("A")})
+
+    for i in fase_3_B_voltam:
+        i.prioridade = 1
+        tempo_aux = i.tempos.get("B")
+        i.tempos.update({"B":random.randint(1,tempo_aux)})
+        i.tempos.update({"B_volta":tempo_aux-i.tempos.get("B")})
+
+    for i in fase_3_C_voltam:
+        i.prioridade = 1
+        tempo_aux = i.tempos.get("C")
+        i.tempos.update({"C":random.randint(1,tempo_aux)})
+        i.tempos.update({"C_volta":tempo_aux-i.tempos.get("C")})
+    # ----------------------------------------------------------------------------------
+
+    for i in utentes: print(i.str())
+    return(utentes)
+
+def adicionar_fila(utente, fila): # adiciona o utente à fila passada como parâmetro
+
+    if(utente.prioridade==0): # utente geral
+        fila[1].append(utente)
+    
+    elif(utente.prioridade==1): # utente prioritário sobre os gerais
+        fila[1].insert(0,utente)
+    
+    else: # utente prioritário
+        fila[0].append(utente)
+
+def escolher_fila(fila): # retorna o próximo utente a ser atendido na fila da fase dada
+
+    if(not fila[0]): # fila prioritária está vazia
+
+        if(not fila[1]): # fila geral também está vazia
+            return(None)
+        
         else:
-            return(list_Fase1_Geral.pop(0))    # devovle primeiro index da lista geral
+            try:
+                return(fila[1].pop(0)) # devolve o primeiro utente da lista geral
+            except: return(None)
+
     else:
-        return(list_Fase1_Prioritaria.pop(0))  # devovle primeiro index da lista prioritaria
+        try:
+            return(fila[0].pop(0))  # devolve o primeiro utente da lista prioritária
+        except: return(None)
 
+def proxima_chegada(utentes): # devolve a próxima chegada ao sistema
 
-def addToFila_Fase2(utente):
-    if(utente.balcao_Fase2 == 1):
-        list_Fase2_BalcaoA.append(utente)
-    elif(utente.balcao_Fase2 == 2):
-        list_Fase2_BalcaoB.append(utente)
-    #elif("ACABAR")
+    proxima = 28800
+    utente = None
 
+    for i in utentes:
+        if(i.estado_atual=="antes" and i.hora_entrada<proxima): 
+            proxima = i.hora_entrada
+            utente = i
 
-# colocar a percentagem como input. Se 1 então saiu dentro da percentagem e 0 se saiu fora
-def random(percent):
-    x = np.random.randint(low = 0, high = 101)
-    print(x)
-    if(x <= percent):
-        return(1)
-    else:
-        return(0)
+    return(proxima,utente)
 
-def totalUtentes():
-    return( np.random.randint(low = 120, high = 151) )
+def proximo_evento(tabela): # calcula o próximo evento a considerar
 
+    minimo = 28800
+    evento = ""
+    utente = tabela.get("utente")
 
+    for k,v in tabela.items():
+        if(k=="clock" or k=="tipo_evento" or k=="utente"): continue
+        if(v<minimo):
+            minimo = v
+            evento = k
+    
+    return(minimo,evento,utente)
 
+def parar_simulacao(tabela): # determina se devemos parar a simulação ou não
 
-# BEGIN
+    parar = True
+    infinito = 100000000
 
-totalUtentes = totalUtentes()
-listaUtentes = []
+    for k,v in tabela.items():
+        if(k=="clock" or k=="tipo_evento" or k=="utente"): continue
+        if(v!=infinito): 
+            parar = False
+            break
+    
+    return(parar)
+
+def calcular_tempos_espera(utentes):
+    # TODO
+    return
+
+def calcular_taxas_ocupacao(utentes):
+    # TODO
+    return
+# -------------------------------------------------------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    num_utentes = total_utentes()
+    gerar_utentes(num_utentes)
--- main.py
+++ main.py
@@ -1,3 +1,248 @@
-//main
+import sys
+from functions import *
 
-//Test1
+# FILAS ([[P], [G]]) E BALCÕES
+# ----------------------------
+fila_fase_1 = [[],[]]
+balcao_Tr = None
+
+fila_fase_2_A = [[],[]]
+balcao_A1 = None
+balcao_A2 = None
+
+fila_fase_2_B = [[],[]]
+balcao_B1 = None
+balcao_B2 = None
+
+fila_fase_2_C = [[],[]]
+balcao_C1 = None
+
+fila_fase_3 = [[],[]]
+balcao_Te = None
+# ----------------------------
+
+num_utentes = total_utentes()
+utentes = gerar_utentes(num_utentes)
+infinito = 100000000
+utente_volta = False
+nova_chegada = (28800,None)
+
+tabela = []
+formato = ["clock", "tipo_evento", "utente", "chegada", "partida_triagem", "partida_balcao_A1", "partida_balcao_A2", "partida_balcao_B1", "partida_balcao_B2", "partida_balcao_C1", "partida_tesouraria"]
+
+tabela.append({"clock": 0, "tipo_evento": "chegada", "utente": proxima_chegada(utentes)[1], "chegada": proxima_chegada(utentes)[0], "partida_triagem": infinito, "partida_balcao_A1": infinito, "partida_balcao_A2": infinito, "partida_balcao_B1": infinito, "partida_balcao_B2": infinito, "partida_balcao_C1": infinito, "partida_tesouraria": infinito})
+
+instante, tipo_evento, utente_atual = proximo_evento(tabela[-1])
+
+balcao_Tr = utente_atual
+utente_atual.estado = "triagem"
+
+print("")
+print("{:<6} | {:<17} | {:<3} | {:<11} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} |".format(" Clock","Evento","Utente","Chegada","Partida tr.","Partida b_A1","Partida b_A2","Partida b_B1","Partida b_B2","Partida b_C1","Partida te."))
+print("----------------------------------------------------------------------------------------------------------------------------------------------------------")
+for i in tabela:
+    if(i.get("tipo_evento")==None):
+        tipo = "None"
+    else: tipo = i.get("tipo_evento")
+    print("{:<6} | {:<17} | {:<6} | {:<11} | {:<11} | {:<12} | {:<12} | {:<12} | {:<12} | {:<12} | {:<11} |".format(i.get("clock"),"---","---",i.get("chegada"),i.get("partida_triagem"),i.get("partida_balcao_A1"),i.get("partida_balcao_A2"),i.get("partida_balcao_B1"),i.get("partida_balcao_B2"),i.get("partida_balcao_C1"),i.get("partida_tesouraria")))
+
+while(not parar_simulacao(tabela[-1])): # simular o sistema
+    
+    if(not utente_volta):
+
+        # COLOCAR ALGUNS DADOS NA TABELA
+        # ---------------------------------------------------------------------------------------------------------------------------------------
+        linha_tabela = {}
+        linha_tabela.update({"clock": instante, "tipo_evento": tipo_evento, "utente": utente_atual, "chegada": proxima_chegada(utentes)[0]})
+        # ---------------------------------------------------------------------------------------------------------------------------------------
+
+    # CHEGADA AO SISTEMA (ENTRADA NA FASE 1)
+    # ----------------------------------------------------------------------------------------
+    if(tipo_evento=="chegada"):
+        
+        if(balcao_Tr==None): # balcão de triagem livre
+            balcao_Tr = utente_atual
+            linha_tabela.update({"partida_triagem": instante + utente_atual.tempos.get("tr")})
+            utente_atual.estado_atual = "triagem"
+
+        else: # balcão de triagem ocupado, o utente vai para a fila 
+            adicionar_fila(utente_atual,fila_fase_1)
+            utente_atual.chegadas_filas.update({"fila_fase_1": instante})
+            utente_atual.estado_atual = "fila_fase_1"
+            linha_tabela.update({"partida_triagem": tabela[-1].get("partida_triagem")})
+        
+        # manter os valores da linha anterior da tabela para os restantes campos
+        for i in formato[5:]: linha_tabela.update({i: tabela[-1].get(i)})
+    # ----------------------------------------------------------------------------------------
+
+    # PARTIDA DA TRIAGEM (ENTRADA NA FASE 2)
+    # -------------------------------------------------------------------------------------------------------------------------------------------
+    elif(tipo_evento=="partida_triagem" or utente_volta==True):
+
+        # ATUALIZAR A PARTE DA TRIAGEM
+        # -----------------------------------------------------------------------------------------
+        if(not utente_volta):
+            balcao_Tr = escolher_fila(fila_fase_1)
+
+            if(balcao_Tr!=None): # balcão da tesouraria livre
+                linha_tabela.update({"partida_triagem": instante + balcao_Tr.tempos.get("tr")})
+                balcao_Tr.estado_atual = "triagem"
+                balcao_Tr.tempo_de_espera += (instante-balcao_Tr.chegadas_filas.get("fila_fase_1"))
+
+            else:
+                linha_tabela.update({"partida_triagem": infinito})
+        # -----------------------------------------------------------------------------------------
+        
+        # COLOCAR O UTENTE NUM DOS BALCÕES (OU FILAS) DA FASE 2
+        # ---------------------------------------------------------------------------------------------------------------------------------------
+        for i in ["A","B","C"]: # avaliar se há algum balcão para receber o utente, se não metê-lo numa fila
+            if(i=="A"): 
+                balcoes = [balcao_A1,balcao_A2]
+                fila = fila_fase_2_A
+            elif(i=="B"): 
+                balcoes = [balcao_B1,balcao_B2]
+                fila = fila_fase_2_B
+            else:
+                balcoes = [balcao_C1]
+                fila = fila_fase_2_C
+            
+            if(utente_atual.tempos.get(i)!=None): # verificar para qual tipo de balcão o utente vai
+                
+                controlo = None
+                for j in balcoes: # verificar se algum dos balcões deste tipo está disponível
+
+                    if(j==None): # balcão livre
+                        j = utente_atual
+                        linha_tabela.update({"partida_balcao_" + i + str(balcoes.index(j)+1): instante + utente_atual.tempos.get(i)})
+                        utente_atual.estado_atual = "balcao_" + i + str(balcoes.index(j)+1)
+                        controlo = i + str(balcoes.index(j)+1)
+                        break
+
+                if(controlo==None): # todos os balcões deste tipo ocupados, o utente vai para a fila
+                    adicionar_fila(utente_atual,fila)
+                    utente_atual.chegadas_filas.update({"fila_fase_2_" + i: instante})
+                    utente_atual.estado_atual = "fila_fase_2_" + i
+                
+                break
+
+        for i in [b for b in ["A1","A2","B1","B2","C1"] if b!=controlo]: # os valores dos balcões para os quais o utente não foi mantêm-se iguais
+            linha_tabela.update({"partida_balcao_" + i: tabela[-1].get("partida_balcao_" + i)})
+
+        if(not utente_atual.direto):
+            linha_tabela.update({"partida_tesouraria": tabela[-1].get("partida_tesouraria")})
+        
+        else: # caso especial para os utentes que vão diretamente da triagem para a tesouraria (fase 1 -> fase 3)
+
+            if(balcao_Te==None): # balcão de tesouraria livre
+                balcao_Te = utente_atual
+                linha_tabela.update({"partida_tesouraria": instante + utente_atual.tempos.get("te")})
+                utente_atual.estado_atual = "tesouraria"
+
+            else: # balcão da tesouraria ocupada, o utente vai para a fila
+                adicionar_fila(utente_atual,fila_fase_3)
+                utente_atual.chegadas_filas.update({"fila_fase_3": instante})
+                utente_atual.estado_atual = "fila_fase_3"
+                linha_tabela.update({"partida_tesouraria": tabela[-1].get("partida_tesouraria")})
+        # ---------------------------------------------------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------------------------------------------------------
+
+    # PARTIDA DO SISTEMA OU RETROCESSO PARA A FASE 2
+    # -------------------------------------------------------------------------------------------
+    elif(tipo_evento=="partida_tesouraria"):
+
+        linha_tabela.update({"partida_triagem": tabela[-1].get("partida_triagem")})
+        for i in ["A1","A2","B1","B2","C1"]: # os valores dos balcões mantêm-se iguais
+            linha_tabela.update({"partida_balcao_" + i: tabela[-1].get("partida_balcao_" + i)})
+
+        # obter o balcão do utente
+        if(utente_atual.tempos.get("A")!=None): balcao = "A"
+        if(utente_atual.tempos.get("B")!=None): balcao = "B"
+        if(utente_atual.tempos.get("C")!=None): balcao = "C"
+
+        if(utente_atual.tempos.get(balcao + "_volta")!=None): # o utente vai voltar para a fase 2
+            utente_volta = True
+            continue
+
+        else: utente_atual.estado_atual = "fora"
+
+        balcao_Te = escolher_fila(fila_fase_3)
+
+        if(balcao_Te!=None):
+            linha_tabela.update({"partida_tesouraria": instante + balcao_Te.tempos.get("te")})
+            balcao_Te.estado_atual = "tesouraria"
+            balcao_Te.tempo_de_espera += (instante-balcao_Te.chegadas_filas.get("fila_fase_3"))
+
+        else:
+            linha_tabela.update({"partida_tesouraria": infinito})
+    # -------------------------------------------------------------------------------------------
+
+    # PARTIDA DE ALGUM BALCÃO A, B OU C
+    # --------------------------------------------------------------------------------------------------
+    else:
+
+        linha_tabela.update({"partida_triagem": tabela[-1].get("partida_triagem")})
+
+        aux = tipo_evento.split("_")[-1]
+
+        # ATUALIZAR A PARTE DOS BALCÕES
+        # ----------------------------------------------------------------------------------------------------------
+        balcoes = [balcao_A1,balcao_A2,balcao_B1,balcao_B2,balcao_C1]
+        balcoes_nomes = ["A1","A2","B1","B2","C1"]
+
+        balcao = balcoes[balcoes_nomes.index(aux)]
+
+        if("A" in aux): fila = fila_fase_2_A
+        elif("B" in aux): fila = fila_fase_2_B
+        else: fila = fila_fase_2_C
+
+        for i in [b for b in ["A1","A2","B1","B2","C1"] if b!=aux]: # os valores dos outros balcões mantêm-se iguais
+            linha_tabela.update({"partida_balcao_" + i: tabela[-1].get("partida_balcao_" + i)})
+
+        balcao = escolher_fila(fila)
+
+        if(balcao!=None):
+            linha_tabela.update({"partida_balcao_" + aux: instante + balcao.tempos.get(str(aux[0]))})
+            balcao.estado_atual = "balcao_" + aux
+            balcao.tempo_de_espera += (instante-balcao.chegadas_filas.get("fila_fase_2_" + str(aux[0])))
+
+        else:
+            linha_tabela.update({"partida_balcao_" + aux: infinito})
+        # ----------------------------------------------------------------------------------------------------------
+
+        # SEGUIR COM O UTENTE QUE SAIU DOS BALCÕES (ENTROU NA FASE 3)
+        # -------------------------------------------------------------------------------------------
+        if(utente_atual.tempos.get("Te")==None): # o utente sai do sistema sem passar pela tesouraria
+            utente_atual.estado_atual = "fora"
+
+        else:
+            if(balcao_Te==None): # balcão de tesouraria livre
+                balcao_Te = utente_atual
+                linha_tabela.update({"partida_tesouraria": instante + utente_atual.tempos.get("te")})
+                utente_atual.estado_atual = "tesouraria"
+            else:
+                adicionar_fila(utente_atual,fila_fase_3)
+                utente_atual.chegadas_filas.update({"fila_fase_3": instante})
+                utente_atual.estado_atual = "fila_fase_3"
+                linha_tabela.update({"partida_tesouraria": tabela[-1].get("partida_tesouraria")})
+        # -------------------------------------------------------------------------------------------
+
+    # atualizar a tabela
+    tabela.append(linha_tabela)
+
+    # escolher o próximo evento
+    instante, tipo_evento, utente_atual = proximo_evento(tabela[-1])
+    utente_volta = False
+
+    if(linha_tabela.get("tipo_evento")==None):
+        tipo = "None"
+    else: tipo = linha_tabela.get("tipo_evento")
+    print("{:<6} | {:<17} | {:<6} | {:<11} | {:<11} | {:<12} | {:<12} | {:<12} | {:<12} | {:<12} | {:<11} |".format(linha_tabela.get("clock"),tipo,(linha_tabela.get("utente")).id,linha_tabela.get("chegada"),linha_tabela.get("partida_triagem"),linha_tabela.get("partida_balcao_A1"),linha_tabela.get("partida_balcao_A2"),linha_tabela.get("partida_balcao_B1"),linha_tabela.get("partida_balcao_B2"),linha_tabela.get("partida_balcao_C1"),linha_tabela.get("partida_tesouraria")))
+
+
+'''
+print("{:<6} | {:<10} | {:<3} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} |".format("Clock","Tipo evento","Utente","Próxima chegada","Partida triagem","Partida balcão A1","Partida balcão A2","Partida balcão B1","Partida balcão B2","Partida balcão C1","Partida tesouraria"))
+for i in tabela:
+    if(i.get("tipo_utente")==None):
+        tipo = "None"
+    else: tipo = i.get("tipo_utente")
+    print("{:<6} | {:<10} | {:<3} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} | {:<6} |".format(i.get("clock"),tipo,(i.get("utente")).id,i.get("proxima_chegada"),i.get("partida_triagem"),i.get("partida_balcao_A1"),i.get("partida_balcao_A2"),i.get("partida_balcao_B1"),i.get("partida_balcao_B2"),i.get("partida_balcao_C1"),i.get("partida_tesouraria")))'''
